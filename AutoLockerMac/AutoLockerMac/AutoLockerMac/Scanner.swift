a19ce9517523b0db2ccedfd262eca2242d2239d1
Andrzej Mistetskij <amystetskyi@blackberry.com>
2019-01-24 18:02:10 +0200

    Partially implemented Scanner

--- AutoLockerMac/AutoLockerMac/AutoLockerMac/Scanner.swift
+++ AutoLockerMac/AutoLockerMac/AutoLockerMac/Scanner.swift
@@ -6,11 +6,63 @@
 //  Copyright © 2019 Andrzej Mistetskij. All rights reserved.
 //
 
+
+import CoreBluetooth
 import Foundation
 
-class Scanner: NSObject,  PeripheralScannable {
+struct DisplayPeripheral: Hashable {
+    let peripheral: CBPeripheral
+    let lastRSSI: NSNumber
+    let isConnectable: Bool
     
+    var hashValue: Int { return peripheral.hashValue }
+    
+    static func ==(lhs: DisplayPeripheral, rhs: DisplayPeripheral) -> Bool {
+        return lhs.peripheral == rhs.peripheral
+    }
+}
+
+class Scanner: NSObject {
+    
+    private var centralManager: CBCentralManager!
+    private var peripherals = Set<DisplayPeripheral>()
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+        
+    }
+}
+
+extension Scanner: PeripheralScannable
+{
     func scanPeripherals() {
         print("start scanning")
+        peripherals = []
+        self.centralManager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) { [weak self] in
+            guard let strongSelf = self else { return }
+            if strongSelf.centralManager!.isScanning {
+                strongSelf.centralManager?.stopScan()
+            }
+            
+        }
+    }
+    
+}
+
+extension Scanner: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if (central.state == .poweredOn) {
+            print("state: On")
+            scanPeripherals()
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        let isConnectable = advertisementData["kCBAdvDataIsConnectable"] as! Bool
+        let discoveredPeripheral = DisplayPeripheral(peripheral: peripheral, lastRSSI: RSSI, isConnectable: isConnectable)
+        peripherals.insert(discoveredPeripheral)
+        
     }
 }
